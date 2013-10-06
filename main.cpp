405b44db7c127c6e0d5edb7215e3451e74537dab
Benedikt Klotz <benedikt.knoppix@web.de>
2013-10-06 18:06:26 +0200

    remove redundant main.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/Editor.hpp"
-
-int main(int argc, char *argv[])
-{
-    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "SimEditor");
-    Editor Window("SimEditor.glade", app);
-}
