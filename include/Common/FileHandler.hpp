b77311410fec13c153d56e10d4c0f5c847e8b366
Manuel <manuel@manuel-ThinkPad-E525.(none)>
2013-10-09 12:12:08 +0200

    Implemented some logic for file handler, still dummy.

--- include/Common/FileHandler.hpp
+++ include/Common/FileHandler.hpp
@@ -1,14 +1,25 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include "../Simulator/Area.hpp"
 
 class FileHandler
 {
     public:
         FileHandler();
         ~FileHandler();
-    protected:
+        bool writeArea(std::string fileName, ClArea *pArea);
+
+
     private:
+        bool createFile(const char *fileName);
+        bool writeLevelDetails(ClArea *pArea);
+        bool writeStaticObjects(ClArea *pArea);
+        bool closeFile();
+        std::ofstream myFile;
 };
 
 #endif // FILEHANDLER_H
--- src/Common/FileHandler.cpp
+++ src/Common/FileHandler.cpp
@@ -2,10 +2,47 @@
 
 FileHandler::FileHandler()
 {
-    //ctor
+
 }
 
 FileHandler::~FileHandler()
 {
     //dtor
+}
+
+bool FileHandler::createFile(const char *fileName){
+    myFile.open(fileName);
+    return 0;
+}
+
+bool FileHandler::writeLevelDetails(ClArea *pArea){
+return 0;
+}
+
+bool FileHandler::writeStaticObjects(ClArea *pArea){
+    int x = pArea->getNumberOfStaticObjects();
+    sf::Vector2f sOPosition;
+    sf::Vector2f sOSize;
+    float sORotation;
+    int sOType = 0;
+    for(int i = 1; i<= x; i++){
+        sOPosition = pArea->getPosition(i);
+        sOSize = pArea->getSize(i);
+        sORotation = pArea->getRotation(i);
+        //sOType = pArea->getType(i);
+
+    }
+    return 0;
+}
+
+bool FileHandler::closeFile(){
+
+    myFile.close();
+    return 0;
+}
+
+bool FileHandler::writeArea(std::string fileName, ClArea *pArea){
+    createFile(fileName.c_str());
+    writeStaticObjects(pArea);
+    return 0;
 }
