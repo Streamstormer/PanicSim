16544e883abc4fc39f266a5bd54c017ffa90506e
Benedikt Klotz <benedikt.knoppix@web.de>
2013-11-04 18:22:57 +0100

    Limit rotation to 90° steps

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -44,7 +44,7 @@ Editor::Editor(string UiPath, Glib::RefPtr<Gtk::Application> app) :
 
     pSizeX->set_adjustment(Gtk::Adjustment::create(10.0, 10.0, 500.0, 0.1, 0.1));
     pSizeY->set_adjustment(Gtk::Adjustment::create(10.0, 10.0, 500.0, 0.1, 0.1));
-    pRot->set_adjustment(Gtk::Adjustment::create(0.0, 0.0, 360.0, 0.1, 0.1));
+    pRot->set_adjustment(Gtk::Adjustment::create(0.0, 0.0, 360.0, 90.0, 90.0));
 
     pAreaX->set_adjustment(Gtk::Adjustment::create(748.0, 748.0, 4000.0, 1.0, 1.0));
     pAreaY->set_adjustment(Gtk::Adjustment::create(710.0, 710.0, 4000.0, 1.0, 1.0));
