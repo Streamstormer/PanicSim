7936cc133757d3dca5727901350a8d72b667ba43
vschwart <victor.schwartz@gmx.de>
2013-11-09 11:42:24 +0100

    Fire is burning with Wrong Picture

--- include/Simulator/StaticObject.hpp
+++ include/Simulator/StaticObject.hpp
@@ -11,7 +11,7 @@ const unsigned int MAXSTATICOBJECTTYPES = GATE; // set this enum to the rightmos
 class ClStaticObject
 {
 public:
-    ClStaticObject(sf::RectangleShape *Rectconst, int id, enum staticObjects Type);
+    ClStaticObject(sf::RectangleShape *Rectconst, int id, enum staticObjects Type,const sf::Texture &texture_fire);
     ~ClStaticObject();
     void draw(sf::RenderWindow& window);
     const sf::Vector2f &getCenter();
@@ -24,10 +24,7 @@ public:
     enum staticObjects getType();
     const sf::Vector2f &getPosition();
     const sf::Vector2f &getSize();
-    void startToBurn()
-    {
-        Rect->setFillColor(sf::Color::Magenta);
-    }
+    void startToBurn();
 
     float getRotation();
 
@@ -46,6 +43,16 @@ private:
     sf::Text text;
     sf::Font font;
     sf::RectangleShape line;
+    sf::Rect<int> subrectFire;
+    sf::Sprite fire_sprite;
+    sf::Clock fire_clock;
+    void subrecttoNumber(int number);
+    int bildID;
+
+    bool isOnFire;
+    const static int PICTUREDURATION =25;
+    const static int ANIMATIONSQUARE =8;
+
 
 };
 
