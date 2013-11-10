7e5ebe382eef28ea92bb349ebea987b24d1db62c
vschwart <victor.schwartz@gmx.de>
2013-11-10 18:41:25 +0100

    Fire can not be placed on GATE and FENCE

--- src/Simulator/Area.cpp
+++ src/Simulator/Area.cpp
@@ -98,7 +98,7 @@
     {
         for( unsigned int n=0; n < sobjects.size(); n++)
         {
-            if(sobjects[n]->getType() != GATE && sobjects[n]->Intersects(point))
+            if(sobjects[n]->getType() != GATE &&sobjects[n]->getType() != FENCE && sobjects[n]->Intersects(point))
             {
                 return false;
             }
