1c25396f69874503bab51d1f844eb4da956dbf45
Einalem <Melanie.Hammerschmidt@online.de>
2013-11-10 19:12:09 +0100

    pathfinding changes

--- src/Simulator/Area.cpp
+++ src/Simulator/Area.cpp
@@ -188,26 +188,33 @@
 
 const sf::Vector2f ClArea::getClosestExit(const sf::Vector2f & myPosition)
 {
-    std::cerr << "Call of closestExit Y: " << myPosition.x << ", Y: " << myPosition.y << " \n";
+    //std::cerr << "Call of closestExit Y: " << myPosition.x << ", Y: " << myPosition.y << " \n";
     float distance = INFINITY;
     sf::Vector2f closestExitPosition;
-    ClStaticObject *closestExit;
     for(unsigned int n = 0; n < sobjects.size(); n++)
     {
         if (sobjects[n]->getType() == GATE)
         {
+            std::cerr<<"Exit "<< n <<std::endl;
             sf::Vector2f position = sobjects[n]->getCenter();
-            float testDistance = (myPosition.x - position.x)*(myPosition.x - position.x)+(myPosition.y - position.y)*(myPosition.x - position.x);
+            std::cerr<<"Position Exit: "<<position.x<<" "<<position.y<<std::endl;
+            float testDistance = (myPosition.x - position.x)*(myPosition.x - position.x)+(myPosition.y - position.y)*(myPosition.y - position.y);
+            std::cerr<<"Position Crowd: "<<myPosition.x<<" "<<myPosition.y<<std::endl;
+            testDistance = sqrt(testDistance);
+            std::cerr<<"testDistance"<< testDistance<<std::endl;
             if (testDistance<distance)
             {
                 closestExitPosition = position;
                 distance = testDistance;
-                closestExit = sobjects[n];
+     //           closestExit = sobjects[n];
             }
-
+            std::cerr<<" " <<std::endl;
         }
     }
-    std::cerr << "Exit middle: " << closestExitPosition.x << ", " << closestExitPosition.y << "\n";
+    std::cerr<<"closest: "<<closestExitPosition.x<<" "<<closestExitPosition.y<<std::endl;
+    return closestExitPosition;
+/*
+   // std::cerr << "Exit middle: " << closestExitPosition.x << ", " << closestExitPosition.y << "\n";
     int numOfExitPoints = closestExit->getSize().x / (EXIT_POINT_DISTANCE + 1);
 
     //Big exit with at minimum two exit points: choose nearest
@@ -278,6 +285,7 @@ const sf::Vector2f ClArea::getClosestExit(const sf::Vector2f & myPosition)
         //Small exit with just one exit point, so no further checks are necessary
         return closestExitPosition;
     }
+    */
 }
 
 
